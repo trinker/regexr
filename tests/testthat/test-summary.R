7783899460d97a849c620f2f2735bf7a0827f31f
Tyler <tyler.rinker@gmail.com>
2014-12-28 12:15:41 -0500

    added unit tests for print.summary and print.unglued

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -19,3 +19,14 @@ test_that("summary outputs pieces needed for printing",{
 })
 
 
+test_that("summary prints as expected",{
+
+    minimal <- structure("abc", class = c("regexr", "character"), regex = list(
+        "a", "b", structure("c", class = c("regex", "character"), 
+            comment = "A love note to your future self")), comments = list(
+        NULL, NULL, "A love note to your future self"))
+
+    expect_equivalent(capture.output(regexr:::print.regexr(minimal)),
+        "[1] \"abc\"")
+
+})
--- tests/testthat/test-unglue.R
+++ tests/testthat/test-unglue.R
@@ -15,4 +15,14 @@ test_that("unglue extracts elements chunks from a regecr object",{
 
 })
 
+test_that("unglue prints as expected",{
 
+    minimal_unglue <- structure(list("a", "b", structure("c", class = c("regex", "character"
+        ), comment = "A love note to your future self")), class = c("unglued", 
+        "list"))
+
+    expect_equivalent(capture.output(regexr:::print.unglued(minimal_unglue)),
+        c("[[1]]", "[1] \"a\"", "", "[[2]]", "[1] \"b\"", "", "[[3]]", 
+        "[1] \"c\"", ""))
+
+})
