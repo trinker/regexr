213c1f972036c9be9ee1d240918609f318de5250
Tyler <tyler.rinker@gmail.com>
2014-12-31 17:40:35 -0500

    updated README typo

--- inst/functions_table/functions.R
+++ inst/functions_table/functions.R
@@ -4,7 +4,7 @@
 | `%:)%`  | Add Comments to a Sub-expressions Within `construct` | 
 | `unglue`  | Break *Concatenated Regex* Into *Sub-expressions* | 
 | `test`   |  Test Validity of *Concatenated Regex* & *Sub-expressions* |
-| `subs`   |  Get/Set Sub-expressions in a `regexr` object |
-| `comments`   |  Get/Set Comments of Sub-expressions in a `regexr` object |
-| `names`   |  Get/Set Names of Sub-expressions in a `regexr` object |
-| `as.regexr`| Coerce Existing Regular Expressions to `regexr` object |
+| `subs`   |  Get/Set Sub-expressions in a `regexr` Object |
+| `comments`   |  Get/Set Comments of Sub-expressions in a `regexr` Object |
+| `names`   |  Get/Set Names of Sub-expressions in a `regexr` Object |
+| `as.regexr`| Coerce Existing Regular Expressions to `regexr` Object |
